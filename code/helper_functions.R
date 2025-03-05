8fdac8f6ebb03ef2665fae0050142f6279454846
Christopher He <he.christopher@gmail.com>
2025-03-04 16:08:14 -0800

    neuronal subclustering notebook, rna_velocity updates

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -76,3 +76,18 @@ idents <- c(
   "immune_cell"
 )
 names(colors_broad) <- idents
+
+colors_timepoints <- c("25som"= "#E496EF", "25som_1"= "#E496EF",
+                       "hb"= "#800080", "hb_1"= "#800080", "hb_2"= "#800080",
+                       "dia1d"= "#C6DBEF", "dia1d_1"= "#C6DBEF",
+                       "dia2d"= "#C6DBEF", "dia2d_1"= "#C6DBEF",
+                       "dia6d"= "#6BAED6", "dia6d_1"= "#6BAED6", "dia6d_2"= "#6BAED6",
+                       "dia1mo"= "#2171B5",  "dia1mo_1"= "#2171B5",
+                       "dia3mo"= "#08306B",  "dia3mo_1"= "#08306B",
+                       "dev1d"= "#FD8D3C", "dev1d_1"= "#FD8D3C", "dev1d_2"= "#FD8D3C", "dev1d_3"= "#FD8D3C",
+                       "predia" = "#800080",
+                       "earlydia" = "#C6DBEF",
+                       "middia" = "#6BAED6", 
+                       "latedia" = "#08306B",
+                       "dev" = "#FD8D3C"
+                       )
