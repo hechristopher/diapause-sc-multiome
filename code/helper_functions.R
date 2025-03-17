a313171228a02abb22def3acb333be7de5346d4a
Christopher He <he.christopher@gmail.com>
2025-03-17 13:23:04 -0700

    small plotting changes

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -1,6 +1,7 @@
 # helper_functions.R
 
 # translate gene orthologs between species
+# acceptable column names: "N. furzeri (NCBI)", "N. furzeri Final Symbol", "
 translate <- function(genes, from = "N. furzeri (NCBI)", to = "N. furzeri Final Symbol") {
   orthos <- readxl::read_excel("C:\\Users\\Christopher He\\OneDrive\\UCSF\\Singh Lab\\gene_ortholog_translation_data.xlsx")
   
@@ -42,40 +43,49 @@ runSeuratRNAProcessing <- function(so, resolution = 0.8, ndims = 30){
 
 colors_broad = Polychrome::palette36.colors(31)
 colors_broad <- Polychrome::sortByHue(colors_broad)
-idents <- c(
-  "myotome/muscle",
-  "head_kidney",
-  "somite",
-  "tail_bud",
-  "endothelial",
-  "blood",
-  "neuron_sema3c",
-  "neuron_dscam",
-  "neuron",
-  "differentiating_neuron",
-  "spinal_cord",
-  "floor_plate", #ATAC ident
-  "neuron_progenitor",
-  "brain", #ATAC ident
-  "midbrain_hindbrain_pax2/5_en1/2",
-  "midbrain_hindbrain_wnt1/lmx1b",
-  "midbrain_hindbrain", #ATAC ident
-  "telencephalon/optic_cup",
-  "hibernating_unknown",
-  "epidermal",
-  "neural_crest",
-  "periderm",
-  "otic_vesicle",
-  "primitive_gut",
-  "pronephros",
-  "hatching_gland",
-  "pigment_cell",
-  "notochord",
-  "fin_bud",
-  "primitive_heart",
-  "immune_cell"
+colors_broad <- c(
+  "neuron_sema3c" = "#66FF00",
+  "neuron_dscam" = "#C0FF00",
+  "neuron" = "#32CD32",
+  "differentiating_neuron" = "#ACE1AF",
+
+  "neuron_progenitor" = "#13EEA4",
+  "brain" = "#458B74", #ATAC ident
+  "spinal_cord" = "#6959CD",
+  "floor_plate" = "#177245", #ATAC ident
+  "telencephalon/optic_cup" = "#698B22",
+  "midbrain_hindbrain_pax2/5_en1/2" = "#008B8B",
+  "midbrain_hindbrain_wnt1/lmx1b" = "#8DEEEE",
+  "midbrain_hindbrain" = "#008B8B", #ATAC ident
+ 
+  "somite" = "#FF7256",
+  "head_kidney" = "#BF5700",
+  "myotome/muscle" = "#CD2626",
+  "primitive_heart" = "#8B0000",
+  "tail_bud" = "#FFA54F",
+  "neural_crest" = "#CDCD00",
+  "pigment_cell" = "#CDCD00",
+
+  "endothelial" = "#DAA520",
+  "blood" = "#FFEC8B",
+  "immune_cell" = "#8B814C",
+
+  "epidermal" = "#F72585",
+  "periderm" = "#7209B7",
+  "otic_vesicle" = "#3A0CA3",
+  "primitive_gut" = "#FF91AF",
+  "pronephros" = "#C21E56",
+
+  "fin_bud" = "#FFDEAD",
+  "notochord" = "#355CBC",
+  "hatching_gland" = "#536878",
+  "hibernating_unknown" = "#C0C0C0"
 )
-names(colors_broad) <- idents
+
+
+# 
+# names(colors_broad) <- idents
+# colors_broad["hatching_gland"] <- "slategrey"
 
 colors_timepoints <- c("25som"= "#E496EF", "25som_1"= "#E496EF",
                        "hb"= "#800080", "hb_1"= "#800080", "hb_2"= "#800080",
